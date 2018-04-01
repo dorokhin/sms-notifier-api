a886c0dd91d35fa438a99c3ab1be165d72149345
Julien Moorat <contact@julienmoorat.fr>
2018-04-01 22:34:28 +0200

    Added API documentation at /docs
    Documentation use apiDoc

---
+++ public/docs/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "SMS Notifier API",
+  "version": "1.0.0",
+  "description": "API for sending SMS through the \"REST SMS Gateway\" Android application",
+  "template": {
+    "forceLanguage": "en",
+    "withCompare": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-04-01T20:32:54.422Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,49 @@
-const router = require('express').Router();
+const express = require('express')
+const path = require('path');
+const router = express.Router();
 const SMSController = require('../controllers/SMSController');
 const StatusController = require('../controllers/StatusController');
 
-// SMS
+/**
+ * @api {post} /sms Send an SMS message
+ * @apiGroup SMS
+ * @apiParam {String} phone Phone number
+ * @apiParam {String} message Message to send
+ * @apiParamExample {json} Input
+ *      {
+ *          "phone": "0601010101",
+ *          "message": "Hi !"
+ *      }
+ * @apiSuccessExample {json} Success
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "message": "message sent"
+ *      }
+ */
 router.post('/sms', SMSController.sendSMS);
 
-// Status
-router.get('/status', StatusController.SMSGatewayStatus)
+/**
+ * @api {get} /status Retrieve status information from the SMS gateway
+ * @apiGroup Status
+ * @apiSuccessExample {json} Success
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "timestamp": 1522613750000,
+ *          "is_airplane_mode": false,
+ *          "telephony": {
+ *              "network_operator_name": "Some operator name",
+ *              "sim_state": "ready",
+ *              "is_network_roaming": false
+ *          },
+ *          "battery": {
+ *              "status": "charging",
+ *              "level": 99
+ *          }
+ *      }
+ */
+router.get('/status', StatusController.SMSGatewayStatus);
+
+// Route for documentation
+router.use('/docs', express.static(path.join(__dirname, '/../public/docs')));
 
 module.exports = router;

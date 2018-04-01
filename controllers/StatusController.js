84d77d786a1bdc57ccd671af9724a58141eb7b12
Julien Moorat <contact@julienmoorat.fr>
2018-04-01 15:15:21 +0200

    Added status endpoint

---
+++ controllers/StatusController.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const checkGatewayAvailability = require('../helpers/checkGatewayAvailability');
+
+const SMSGatewayStatus = async (req, res) => {
+    if(await checkGatewayAvailability()) {
+        const gatewayStatusUrl = 'http://' + process.env.MOBILE_PHONE_IP_ADDR + ':' + process.env.MOBILE_PHONE_PORT + '/v1/device/status';
+        try {
+            let response = await axios.get(gatewayStatusUrl);
+            if(response.status === 200) {
+                res.json(response.data);
+            }
+            else {
+                res.status(500).json({error: "Unexpected error"});
+            }
+        }
+        catch(e) {
+            res.status(500).json({error: "Error while contacting SMS Gateway"});
+        }
+    }
+    else {
+        res.status(500).json({
+            error: "SMS Gateway is unreachable"
+        });
+    }
+}
+module.exports.SMSGatewayStatus = SMSGatewayStatus;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const SMSController = require('../controllers/SMSController');
+const StatusController = require('../controllers/StatusController');
 
 // SMS
 router.post('/sms', SMSController.sendSMS);
+
+// Status
+router.get('/status', StatusController.SMSGatewayStatus)
 
 module.exports = router;
